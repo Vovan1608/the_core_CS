d65f31b924a347c3588e97afc5a3b3d398344860
Vovan1608 <vovan1608@gmail.com>
2021-03-28 11:06:51 +0300

    added base code

---
+++ #18_Array Packing/main.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/* 
+You are given an array of up to four non-negative integers, each less than 256.
+
+Your task is to pack these integers into one number M in the following way:
+
+The first element of the array occupies the first 8 bits of M;
+The second element occupies next 8 bits, and so on.
+Return the obtained integer M.
+
+Note: the phrase "first bits of M" refers to the least significant bits of M - the right-most bits of an integer. For further clarification see the following example.
+
+Example
+
+For a = [24, 85, 0], the output should be
+arrayPacking(a) = 21784.
+
+An array [24, 85, 0] looks like [00011000, 01010101, 00000000] in binary.
+After packing these into one number we get 00000000 01010101 00011000 (spaces are placed for convenience), which equals to 21784
+*/
+
+function arrayPacking(a) {
+
+}
+
+const a = [24, 85, 0];
+
+const add = document.querySelector("#add");
+add.style = `
+	background-color: #f1f5f8;
+	font-size: 36px;
+	text-align: center;
+	border: 2px solid #000;
+	border-radius: 7px;
+	box-shadow: inset 2px 2px 5px rgba(154, 147, 140, 0.5), 1px 1px 5px rgba(255, 255, 255, 1);
+`;
+
+const el = document.createElement("div");
+el.innerText = arrayPacking(a);
+
+document.body.style.backgroundColor = "#61dafb";
+
+add.append(el);
