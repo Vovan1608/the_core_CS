124e6a9e64aa6ca887e642cf1be0945fc2a889a3
Vovan1608 <vovan1608@gmail.com>
2021-03-30 13:02:59 +0300

    done #23

--- #23_Different Rightmost Bit/main.js
+++ #23_Different Rightmost Bit/main.js
@@ -17,9 +17,11 @@ So the answer is 24 = 16.
 */
 
 function differentRightmostBit(n, m) {
-  return getArrayFromNum(n).length === getArrayFromNum(m) ? 
+  return 2 ** isDif(getArrayFromNum(n).length === getArrayFromNum(m) ? 
 	[getArrayFromNum(n), getArrayFromNum(m)] :
-	[getArrIfDif(getArrayFromNum(n), getArrayFromNum(m))];
+	[getArrIfDif(getArrayFromNum(n), getArrayFromNum(m))]);
+
+	// return (n^m) & -(n^m);
 }
 
 const getArrayFromNum = (num) => [...num.toString(2)].reverse();
@@ -29,15 +31,23 @@ const addedZeroAfter = (arr, dif) => arr.join("") + "0".repeat(dif);
 const getDif = (arr1, arr2) => Math.abs(arr1.length - arr2.length);
 
 const getArrIfDif = (arr1, arr2) => arr1.length > arr2.length ?
-	[arr1, addedZeroAfter(arr2, getDif(arr1, arr2))] : 
-	[addedZeroAfter(arr1, getDif(arr1, arr2)), arr2];
+	[...arr1, ...[...addedZeroAfter(arr2, getDif(arr1, arr2))]] :
+	[...[...addedZeroAfter(arr1, getDif(arr1, arr2))], ...arr2];
 
-// 
-const n = 11;
-const m = 13;
+const isDif = (arr) => {
+	const size = arr[0].length / 2;
+	for (let i = 0; i < size; i++) {
+		if (arr[0][i] !== arr[0][i + size]) {
+			console.log(i);
+			return i;
+		}
+	}
+}
+// const n = 11;
+// const m = 13;
 
-// const n = 7;
-// const m = 23;
+const n = 7;
+const m = 23;
 
 document.body.style.backgroundColor = "#61dafb";
 
