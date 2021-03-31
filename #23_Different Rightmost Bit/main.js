571190d35327e36d834088003cab1687300304f9
Vovan1608 <vovan1608@gmail.com>
2021-03-31 19:38:25 +0300

    added coments

--- #23_Different Rightmost Bit/main.js
+++ #23_Different Rightmost Bit/main.js
@@ -2,7 +2,7 @@
 
 /* 
 You're given two integers, n and m. Find position of the rightmost bit in which they differ in their binary representations (it is guaranteed that such a bit exists), counting from right to left.
-Return the value of 2position_of_the_found_bit (0-based).
+Return the value of 2 ** position_of_the_found_bit (0-based).
 Example
 For n = 11 and m = 13, the output should be
 differentRightmostBit(n, m) = 2.
