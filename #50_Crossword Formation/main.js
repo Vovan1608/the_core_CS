99c9a08f9ba624a082e2b4fb169cc43d7db350ad
Vovan1608 <vovan1608@gmail.com>
2021-04-08 13:50:34 +0300

    added function check

--- #50_Crossword Formation/main.js
+++ #50_Crossword Formation/main.js
@@ -27,6 +27,24 @@ function crosswordFormation(words) {
     return t;
 }
 
-function check() {}
+function check(a, b, c, d) {
+	let t = 0;
+    for (let a1 = 0; a1 < a.length; a1++)
+        for (let a2 = a1+2; a2 < a.length; a2++)
+            for (let b1 = 0; b1 < b.length; b1++)
+                for (let b2 = b1+2; b2 < b.length; b2++)
+                    for (let c1 = 0; c1 < c.length; c1++)
+                        for (let d1 = 0; d1 < d.length; d1++) {
+                            let c2 = c1 + (a2-a1);
+                            let d2 = d1 + (b2-b1);
+                            if (c2 < c.length && d2 < d.length) {
+                                if (a.charAt(a1) === b.charAt(b1)
+																	&& a.charAt(a2) === d.charAt(d1)
+																	&& c.charAt(c1) === b.charAt(b2)
+																	&& c.charAt(c2) === d.charAt(d2)) t++;
+                            }
+                        }
+    return t;
+}
 
 const words = ["crossword", "square", "formation", "something"];
