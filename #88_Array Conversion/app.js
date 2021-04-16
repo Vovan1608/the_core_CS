98f440508daabe88becacdd58893786e4dce0eb7
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-16 11:56:48 +0300

    added base code

---
+++ #88_Array Conversion/app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/* 
+Given an array of 2k integers (for some integer k), perform the following operations until the array contains only one element:
+
+On the 1st, 3rd, 5th, etc. iterations (1-based) replace each pair of consecutive elements with their sum;
+On the 2nd, 4th, 6th, etc. iterations replace each pair of consecutive elements with their product.
+After the algorithm has finished, there will be a single element left in the array. Return that element.
+
+Example
+
+For inputArray = [1, 2, 3, 4, 5, 6, 7, 8], the output should be
+arrayConversion(inputArray) = 186.
+
+We have [1, 2, 3, 4, 5, 6, 7, 8] -> [3, 7, 11, 15] -> [21, 165] -> [186], so the answer is 186.
+*/
+
+function arrayConversion(inputArray) {
+	return inputArray;
+}
+
+const inputArray = [1, 2, 3, 4, 5, 6, 7, 8]; // -> 186
+
+document.body.style.backgroundColor = "#61dafb";
+
+const app = document.getElementById("app");
+app.style = `
+	width: 50%;
+	font-size: 36px;
+	text-align: center;
+	margin: 200px auto;
+	color: #222;
+	border: 2px solid #222;
+	background-color: #f1f5f8;
+	border-radius: 7px;
+	box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+`;
+
+const el = document.createElement("div");
+el.textContent = arrayConversion(inputArray);
+
+app.append(el);
